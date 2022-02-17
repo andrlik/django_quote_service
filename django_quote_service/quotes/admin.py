3822f67dfd1144b48c1ca1b09632c7019e839bcc
Daniel Andrlik <daniel@andrlik.org>
2022-02-17 15:46:27 -0500

    Add admin views and convenience link for superusers

--- django_quote_service/quotes/admin.py
+++ django_quote_service/quotes/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from .models import Character, CharacterGroup, CharacterMarkovModel, Quote
+from .models import (
+    Character,
+    CharacterGroup,
+    CharacterMarkovModel,
+    CharacterStats,
+    GroupMarkovModel,
+    GroupStats,
+    Quote,
+    QuoteStats,
+)
 
 # Register your models here.
 
@@ -21,7 +30,27 @@ class CharacterMarkovModelAdmin(admin.ModelAdmin):
     pass
 
 
+class GroupMarkovModelAdmin(admin.ModelAdmin):
+    pass
+
+
+class GroupStatsAdmin(admin.ModelAdmin):
+    pass
+
+
+class CharacterStatAdmin(admin.ModelAdmin):
+    pass
+
+
+class QuoteStatAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(CharacterGroup, CharacterGroupAdmin)
 admin.site.register(Character, CharacterAdmin)
 admin.site.register(Quote, QuoteAdmin)
 admin.site.register(CharacterMarkovModel, CharacterMarkovModelAdmin)
+admin.site.register(GroupMarkovModel, GroupMarkovModelAdmin)
+admin.site.register(GroupStats, GroupStatsAdmin)
+admin.site.register(CharacterStats, CharacterStatAdmin)
+admin.site.register(QuoteStats, QuoteStatAdmin)
