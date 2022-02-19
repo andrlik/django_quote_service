dd2ba7204ab7261027a8f8e1bba4e9e6a8810252
Daniel Andrlik <daniel@andrlik.org>
2022-02-19 13:44:28 -0500

    Improve serializer output

--- django_quote_service/quotes/api/serializers.py
+++ django_quote_service/quotes/api/serializers.py
@@ -3,19 +3,23 @@ from rest_framework.serializers import ModelSerializer
 from ..models import Character, CharacterGroup, Quote
 
 
-class CharacterSerializer(ModelSerializer):
-    class Meta:
-        model = Character
-        fields = ["name", "group", "slug", "description", "description_rendered"]
-
-
 class CharacterGroupSerializer(ModelSerializer):
     class Meta:
         model = CharacterGroup
         fields = ["name", "slug", "description", "description_rendered"]
 
 
+class CharacterSerializer(ModelSerializer):
+    group = CharacterGroupSerializer()
+
+    class Meta:
+        model = Character
+        fields = ["name", "group", "slug", "description", "description_rendered"]
+
+
 class QuoteSerializer(ModelSerializer):
+    character = CharacterSerializer()
+
     class Meta:
         model = Quote
         fields = ["quote", "quote_rendered", "character"]
