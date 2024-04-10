8dddbbabbd85c07cc64a3e63efe7c5ae53a076a9
Daniel Andrlik <daniel@andrlik.org>
2024-04-10 11:53:25 +0200

    Add django-watchfiles and add back in pytest-django

--- config/settings/local.py
+++ config/settings/local.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa F405
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
 if DEBUG:
-    INSTALLED_APPS += ["debug_toolbar"]  # F405
+    INSTALLED_APPS += ["debug_toolbar", "django_watchfiles"]  # F405
     # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
     MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
     # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
