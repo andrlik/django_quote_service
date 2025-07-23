3f2e7f3fddaa60298ad950ef21bb4f08eea4fbcd
Daniel Andrlik <daniel@andrlik.org>
2025-07-23 13:25:16 -0400

    Move staticfiles collection to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,6 @@ set -eo pipefail
 export PYTHONUNBUFFERED=1
 
 python -m manage migrate --noinput --skip-checks
+python -m manage collectstatic --noinput --skip-checks
 
 uvicorn --host 0.0.0.0 --port 8000 --proxy-headers config.asgi:application
